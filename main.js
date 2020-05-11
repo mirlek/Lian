b29ca4381e5a0d79b3268919829a5877ca2794f3
Ekaterina <57844753+mirlek@users.noreply.github.com>
2020-05-11 15:39:56 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+ 
+
+$(document).ready(function(){
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 80) {
+				$(".header").addClass("header-sticky");
+			} else {
+				$('.header').removeClass("header-sticky");
+			}
+		});
+  });
+  
+  $(function(){
+    $('.burger_lines').click(function(){
+        $(this).toggleClass('burger_cross');
+        $('.nav_bar').toggleClass('nav_bar_toggle');
+        $('.nav').toggleClass('nav_toggle');
+        $('.nav__item').toggleClass('nav__item_toggle');
+        
+    })
+  });
+
+});
+
+
+
+
+
