80fdf8bb83324525d5827475678f58b702b35624
Ekaterina <57844753+mirlek@users.noreply.github.com>
2020-06-21 17:19:45 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -21,9 +21,59 @@ $(document).ready(function(){
     })
   });
 
+  $(function(){
+  $( '.post' ).hover(function() {
+      $( this ).toggleClass( "post_clone" );
+      
+    }
+ 
+  );
+  
 });
 
 
+$(function(){
+  $('.posts').hover(function(){
+    $(this).toggleClass("hide");
+    
+  }  
+);
+/* $('.ost').hover(function(){
+  $(this).toggleClass("fact");
+}  
+);
+ */
+});
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 
 
 
+
+});
